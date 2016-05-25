6b8797acafd2acfc4da926972e6387ca4b386c52
texasenglish <texasenglish@utexas.edu>
2016-05-25 14:24:54 -0500

    Delete untitled text 3.py

--- untitled text 3.py
+++
@@ -1,7 +0,0 @@
-import codecs
-
-try:
-	codecs.lookup("trr")
-except LookupError, err:
-	print "Make sure you have picked the right encoding for your file.\nIt is currently set to {}. Common formats are \'utf-8\' or \'ascii\'.".format("assi")
-	raise LookupError, err
